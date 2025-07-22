b6e474d5c0ff682a1b4703903ee4361f33b21bd9
mano299 <mano299ahmed1@gmail.com>
2025-07-22 11:30:24 +0300

    create custom app bar & added it to home view

---
+++ lib/features/home/presention/widgets/custom_home_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:bookly/core/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomHomeAppBar extends StatelessWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            AssetsData.logo,
+            height: 20,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
