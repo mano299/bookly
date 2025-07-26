42057df80cab251e263a6059154480f1c7546f9d
mano299 <mano299ahmed1@gmail.com>
2025-07-26 16:27:42 +0300

    create book details view body

--- lib/features/home/presention/widgets/book_details_view_body.dart
+++ lib/features/home/presention/widgets/book_details_view_body.dart
@@ -1,12 +1,18 @@
+import 'package:bookly/features/home/presention/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
-import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SafeArea(child: CustomBookDetailsAppBar()),
+        ],
+      ),
+    );
   }
 }
